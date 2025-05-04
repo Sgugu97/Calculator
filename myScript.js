5c0f51e30cfa730adc6c437285b107011beb50a6
sgugu <188474125+Sgugu97@users.noreply.github.com>
2025-05-04 19:20:44 +0200

    add operators and 'variables'

--- myScript.js
+++ myScript.js
@@ -2,12 +2,46 @@ const keyBoard = document.createElement("div");
 keyBoard.setAttribute("class", "keyBoard");
 document.body.appendChild(keyBoard);
 
+const operators = document.createElement("div");
+operators.setAttribute("class", "operators");
+keyBoard.appendChild(operators);
+
+const simbols =["/","*","-","+","="];
+for (let i=0; i<simbols.length; i++){
+    const operator =document.createElement("button");
+    operator.setAttribute("class", "operator");
+    operator.textContent = simbols[i];
+    operators.appendChild(operator);
+    operator.addEventListener("click", ()=> console.log(simbols[i]));
+}
+
+
+const varies = document.createElement("div");
+varies.setAttribute("class", "varies");
+keyBoard.appendChild(varies);
+
+const variables = ["AC", "+/-", "%"];
+for (let i=0; i<variables.length; i++){
+    const variable =document.createElement("button");
+    variable.setAttribute("class", "variable");
+    variable.textContent = variables[i];
+    varies.appendChild(variable);
+    variable.addEventListener("click", ()=> console.log(variables[i]));
+}
+
+const digits = document.createElement("div");
+digits.setAttribute("class", "digits");
+varies.appendChild(digits);
+
+const nums = document.createElement("div");
+nums.setAttribute("class", "nums");
+digits.appendChild(nums);
 
 for(let i=1; i<=9; i++){
     const num = document.createElement("button");
     num.setAttribute("class", `num`);
     num.textContent = `${i}`;
-    keyBoard.appendChild(num);
+    nums.appendChild(num);
     num.addEventListener("click", ()=> console.log(`${i}`));
 }
 
@@ -15,14 +49,14 @@ for(let i=1; i<=9; i++){
 const zero = document.createElement("button");
 zero.setAttribute("class", `num zero`);
 zero.textContent = `0`;
-keyBoard.appendChild(zero);
+digits.appendChild(zero);
 zero.addEventListener("click", ()=> console.log(`0`));
 
 
 const point = document.createElement("button");
 point.setAttribute("class", `num point`);
 point.textContent = `.`;
-keyBoard.appendChild(point);
+digits.appendChild(point);
 point.addEventListener("click", ()=> console.log(`.`));
 
 
