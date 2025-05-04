3480fa4be36be305d23e081a7a306455b0cf89c0
sgugu <188474125+Sgugu97@users.noreply.github.com>
2025-05-04 18:38:45 +0200

    add event listener

--- myScript.js
+++ myScript.js
@@ -1,12 +1,29 @@
-
-
 const keyBoard = document.createElement("div");
 keyBoard.setAttribute("class", "keyBoard");
 document.body.appendChild(keyBoard);
 
-for(let i=1; i<10; i++){
-    const nums = document.createElement("button");
-    nums.setAttribute("class", `num ${i}`);
-    nums.textContent = `${i}`;
-    keyBoard.appendChild(nums);
+
+for(let i=1; i<=9; i++){
+    const num = document.createElement("button");
+    num.setAttribute("class", `num`);
+    num.textContent = `${i}`;
+    keyBoard.appendChild(num);
+    num.addEventListener("click", ()=> console.log(`${i}`));
 }
+
+
+const zero = document.createElement("button");
+zero.setAttribute("class", `num zero`);
+zero.textContent = `0`;
+keyBoard.appendChild(zero);
+zero.addEventListener("click", ()=> console.log(`0`));
+
+
+const point = document.createElement("button");
+point.setAttribute("class", `num point`);
+point.textContent = `.`;
+keyBoard.appendChild(point);
+point.addEventListener("click", ()=> console.log(`.`));
+
+
+
