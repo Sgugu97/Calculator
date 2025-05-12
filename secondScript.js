89de12adde12c2aa0d29b7c0fa1ec0bdf63847f3
sgugu <188474125+Sgugu97@users.noreply.github.com>
2025-05-12 12:40:25 +0200

    implemented possibility to display result for each operation

--- secondScript.js
+++ secondScript.js
@@ -22,18 +22,11 @@ function operate(operator, numFirst, numSecond){
         return divide(numFirst, numSecond);
     }
 }
+let switchForMonitor=true;
+var operand=[];
+var ope;
+var tot=[];
 
-const operand=[];
-let ope;
-const tot=[];
-
-function myFunction(){
-    if(a.lenght<2){
-        a.push(parseInt(monitor.textContent));
-    }else{
-        b.push(parseInt(monitor.textContent));
-    }
-}
 
 const monitor = document.querySelector(".display");
 const nums = document.querySelectorAll(".num");
@@ -41,7 +34,14 @@ const equal= document.querySelector(".equal");
 
 
 nums.forEach(num => {
-    num.addEventListener("click", () => monitor.textContent = parseInt(monitor.textContent + `${num.textContent}`));
+    num.addEventListener("click", () => {
+        if(switchForMonitor===true){
+            monitor.textContent = parseInt(monitor.textContent + `${num.textContent}`);
+        }else if(switchForMonitor===false){
+            monitor.textContent=`${num.textContent}`;
+            switchForMonitor=true;
+        }
+    });
 });
 
 const simbols = document.querySelectorAll(".operator");
@@ -53,15 +53,22 @@ simbols.forEach(simbol => {
         if(operand.length>1){
             if(tot.length>=1){
                 tot.push(operate(ope,tot.slice(-1)[0],operand.slice(-1)[0]))
+                switchForMonitor=false;
+                monitor.textContent=tot.slice(-1)[0];
             }else{
                 tot.push(operate(ope,operand.slice(-2)[0],operand.slice(-1)[0]));
+                switchForMonitor=false;
+                monitor.textContent=tot.slice(-1)[0];
             }
+        }else{
+            switchForMonitor=false;
+            monitor.textContent=0;
         }
-        monitor.textContent=0;
         ope=simbol.textContent;
-        console.log(operand);
-        console.log(tot);
-        console.log(ope);
+        console.log("operand " + " [" + operand + "]");
+        console.log("tot " +  " [" + tot + "]");
+        console.log("ope " +  " [" + ope + "]");
+        console.log(switchForMonitor)
     })
 });
 
@@ -71,9 +78,15 @@ equal.addEventListener("click",()=>{
     operand.push(parseInt(monitor.textContent));
     if(operand.length>1){
         if(tot.length>=1){
-            tot.push(operate(ope,tot.slice(-1)[0],operand.slice(-1)[0]))
+            tot.push(operate(ope,tot.slice(-1)[0],operand.slice(-1)[0]));
+            tot=[tot.slice(-1)[0]];
+            ope="";
+            operand=[]
         }else{
             tot.push(operate(ope,operand.slice(-2)[0],operand.slice(-1)[0]));
+            tot=[tot.slice(-1)[0]];
+            ope="";
+            operand=[]
         }
     }
     monitor.textContent=tot.slice(-1)[0];
