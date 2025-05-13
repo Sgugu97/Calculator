6ba29afa065b47cad3fc316eda0da6a8ab00c96a
sgugu <188474125+Sgugu97@users.noreply.github.com>
2025-05-13 13:43:20 +0200

    monitor display only 9 digits and some minor adjustments

--- secondScript.js
+++ secondScript.js
@@ -27,11 +27,19 @@ var operand=[];
 var ope;
 var tot=[];
 
-
+const point=document.querySelector(".point");
 const monitor = document.querySelector(".display");
 const nums = document.querySelectorAll(".num");
 const equal= document.querySelector(".equal");
+const varies=document.querySelectorAll(".varie")
 
+point.addEventListener("click",()=>{
+    monitor.textContent= `${monitor.textContent}` + ".";
+    console.log("operand " + " [" + operand + "]");
+    console.log("tot " +  " [" + tot + "]");
+    console.log("ope " +  " [" + ope + "]");
+    console.log(switchForMonitor)
+})
 
 nums.forEach(num => {
     num.addEventListener("click", () => {
@@ -41,6 +49,7 @@ nums.forEach(num => {
             monitor.textContent=`${num.textContent}`;
             switchForMonitor=true;
         }
+        monitor.innerHTML=monitor.innerHTML.substring(0,8);
     });
 });
 
@@ -68,7 +77,8 @@ simbols.forEach(simbol => {
         console.log("operand " + " [" + operand + "]");
         console.log("tot " +  " [" + tot + "]");
         console.log("ope " +  " [" + ope + "]");
-        console.log(switchForMonitor)
+        console.log(switchForMonitor);
+        monitor.innerHTML=monitor.innerHTML.toString().substring(0,8);
     })
 });
 
@@ -81,21 +91,89 @@ equal.addEventListener("click",()=>{
             tot.push(operate(ope,tot.slice(-1)[0],operand.slice(-1)[0]));
             tot=[tot.slice(-1)[0]];
             ope="";
-            operand=[]
+            operand=[];
+            monitor.textContent=tot.slice(-1)[0];
+            tot=[];
         }else{
             tot.push(operate(ope,operand.slice(-2)[0],operand.slice(-1)[0]));
             tot=[tot.slice(-1)[0]];
             ope="";
-            operand=[]
+            operand=[];
+            monitor.textContent=tot.slice(-1)[0];
+            tot=[];
         }
+    }else{
+        monitor.textContent=operand;
+        operand=[];
     }
-    monitor.textContent=tot.slice(-1)[0];
-    console.log(operand);
-    console.log(tot);
-    console.log(ope);
+    console.log("operand " + " [" + operand + "]");
+    console.log("tot " +  " [" + tot + "]");
+    console.log("ope " +  " [" + ope + "]");
+    console.log(switchForMonitor)
+    monitor.innerHTML=monitor.innerHTML.toString().substring(0,8);
 });
 
 
 
-
-
+varies.forEach(varie =>{
+    varie.addEventListener("click",()=>{
+        operand.push(parseInt(monitor.textContent));
+        ope=varie.textContent;
+        if (ope==="AC"){
+            switchForMonitor=false;
+            operand=[];
+            ope= "";
+            sim = "";
+            tot=[];
+            monitor.textContent=0;
+        }else if (ope === "%"){
+            if(operand.length > 1){
+                if(tot.length>=1){
+                    tot.push(operate(ope,tot.slice(-1)[0],operand.slice(-1)[0]));
+                    switchForMonitor=false;
+                }else{
+                    tot.push(operate(ope,operand.slice(-2)[0],operand.slice(-1)[0]));
+  
+                }
+                tot=[tot.slice(-1)[0]];
+                monitor.textContent=tot.slice(-1)[0]/100;
+            }else{
+                tot.push(monitor.textContent);
+                monitor.textContent=monitor.textContent/100;
+                tot=[]
+            }
+            operand=[];
+            ope="";
+            monitor.innerHTML=monitor.innerHTML.toString().substring(0,8);
+        }else if(ope === "+/-"){
+            if(operand.length > 1){
+                if(tot.length>=1){
+                    tot.push(operate(ope,tot.slice(-1)[0],operand.slice(-1)[0]));
+                    switchForMonitor=false;
+                    monitor.textContent=tot.slice(-1)[0];
+                    tot=[tot.slice(-1)[0]];
+                    operand=[];
+                    ope="";
+                    monitor.textContent=tot.slice(-1)[0]*-1;
+                }else{
+                    tot.push(operate(ope,operand.slice(-2)[0],operand.slice(-1)[0]));
+                    tot=[tot.slice(-1)[0]];
+                    ope="";
+                    operand=[];
+                    monitor.textContent=tot.slice(-1)[0]*-1;
+                }
+            }else{
+                tot.push(monitor.textContent);
+                monitor.textContent=monitor.textContent*-1;
+                operand=[];
+                ope="";
+                tot=[]
+            }
+        }
+                console.log("operand " + " [" + operand + "]");
+        console.log("tot " +  " [" + tot + "]");
+        console.log("ope " +  " [" + ope + "]");
+        console.log(switchForMonitor);
+        monitor.innerHTML=monitor.innerHTML.toString().substring(0,8);
+    })
+})
