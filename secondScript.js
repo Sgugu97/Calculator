d93e45e3310ba7dee3825f8922564a71be6afb6c
sgugu <188474125+Sgugu97@users.noreply.github.com>
2025-05-13 10:08:20 +0200

    Revert "implement AC % +/-"
    This reverts commit 0082976ed16b65757a19843bbc4c22560785dcb3.

--- secondScript.js
+++ secondScript.js
@@ -1,29 +1,15 @@
-let switchForMonitor=true;
-var operand=[];
-let ope;
-let sim;
-var tot=[];
-
-const monitor = document.querySelector(".display");
-const nums = document.querySelectorAll(".num");
-const equal= document.querySelector(".equal");
-const simbols = document.querySelectorAll(".operator");
-const varies = document.querySelectorAll(".varie");
-const point=document.querySelector(".point");
-
 function add(a,b){
     return a + b;
-};
+}
 function subtract(a,b){
     return a - b;
-};
+}
 function multiply(a,b){
     return a * b;
-};
+}
 function divide(a,b){
     return a / b;
-};
-
+}
 
 function operate(operator, numFirst, numSecond){
     if(operator === `+`){
@@ -36,7 +22,15 @@ function operate(operator, numFirst, numSecond){
         return divide(numFirst, numSecond);
     }
 }
+let switchForMonitor=true;
+var operand=[];
+var ope;
+var tot=[];
 
+
+const monitor = document.querySelector(".display");
+const nums = document.querySelectorAll(".num");
+const equal= document.querySelector(".equal");
 
 
 nums.forEach(num => {
@@ -50,13 +44,15 @@ nums.forEach(num => {
     });
 });
 
+const simbols = document.querySelectorAll(".operator");
+
 
 simbols.forEach(simbol => {
     simbol.addEventListener("click", ()=>{
         operand.push(parseInt(monitor.textContent));
         if(operand.length>1){
             if(tot.length>=1){
-                tot.push(operate(ope,tot.slice(-1)[0],operand.slice(-1)[0]));
+                tot.push(operate(ope,tot.slice(-1)[0],operand.slice(-1)[0]))
                 switchForMonitor=false;
                 monitor.textContent=tot.slice(-1)[0];
             }else{
@@ -80,103 +76,26 @@ simbols.forEach(simbol => {
 
 equal.addEventListener("click",()=>{
     operand.push(parseInt(monitor.textContent));
-    if(operand.length > 1){
+    if(operand.length>1){
         if(tot.length>=1){
             tot.push(operate(ope,tot.slice(-1)[0],operand.slice(-1)[0]));
-            switchForMonitor=false;
-            monitor.textContent=tot.slice(-1)[0];
             tot=[tot.slice(-1)[0]];
-            operand=[];
             ope="";
-            monitor.textContent=tot.slice(-1)[0];
+            operand=[]
         }else{
             tot.push(operate(ope,operand.slice(-2)[0],operand.slice(-1)[0]));
             tot=[tot.slice(-1)[0]];
             ope="";
-            operand=[];
-            monitor.textContent=tot.slice(-1)[0];
+            operand=[]
         }
-    }else{
-        operand=[];
-        ope="";
-        tot=[]
     }
-    console.log("operand " + " [" + operand + "]");
-    console.log("tot " +  " [" + tot + "]");
-    console.log("ope " +  " [" + ope + "]");
-    console.log(switchForMonitor)
+    monitor.textContent=tot.slice(-1)[0];
+    console.log(operand);
+    console.log(tot);
+    console.log(ope);
 });
 
 
 
-varies.forEach(varie =>{
-    varie.addEventListener("click",()=>{
-        operand.push(parseInt(monitor.textContent));
-        ope=varie.textContent;
-        if (ope==="AC"){
-            switchForMonitor=true;
-            operand=[];
-            ope= "";
-            sim = "";
-            tot=[];
-            monitor.textContent=0;
-        }else if (ope === "%"){
-            if(operand.length > 1){
-                if(tot.length>=1){
-                    tot.push(operate(ope,tot.slice(-1)[0],operand.slice(-1)[0]));
-                    switchForMonitor=false;
-                    monitor.textContent=tot.slice(-1)[0];
-                    tot=[tot.slice(-1)[0]];
-                    operand=[];
-                    ope="";
-                    monitor.textContent=parseInt(tot.slice(-1)[0])/100;
-                }else{
-                    tot.push(operate(ope,operand.slice(-2)[0],operand.slice(-1)[0]));
-                    tot=[tot.slice(-1)[0]];
-                    ope="";
-                    operand=[];
-                    monitor.textContent=parseInt(tot.slice(-1)[0])/100;
-                }
-            }else{
-                tot.push(monitor.textContent);
-                monitor.textContent=parseInt(monitor.textContent)/100;
-                operand=[];
-                ope="";
-                tot=[]
-            }
-            console.log("operand " + " [" + operand + "]");
-            console.log("tot " +  " [" + tot + "]");
-            console.log("ope " +  " [" + ope + "]");
-            console.log(switchForMonitor)
-        }else if(ope === "+/-"){
-            if(operand.length > 1){
-                if(tot.length>=1){
-                    tot.push(operate(ope,tot.slice(-1)[0],operand.slice(-1)[0]));
-                    switchForMonitor=false;
-                    monitor.textContent=tot.slice(-1)[0];
-                    tot=[tot.slice(-1)[0]];
-                    operand=[];
-                    ope="";
-                    monitor.textContent=parseInt(tot.slice(-1)[0])*-1;
-                }else{
-                    tot.push(operate(ope,operand.slice(-2)[0],operand.slice(-1)[0]));
-                    tot=[tot.slice(-1)[0]];
-                    ope="";
-                    operand=[];
-                    monitor.textContent=parseInt(tot.slice(-1)[0])*-1;
-                }
-            }else{
-                tot.push(monitor.textContent);
-                monitor.textContent=parseInt(monitor.textContent)*-1;
-                operand=[];
-                ope="";
-                tot=[]
-            }
-            console.log("operand " + " [" + operand + "]");
-            console.log("tot " +  " [" + tot + "]");
-            console.log("ope " +  " [" + ope + "]");
-            console.log(switchForMonitor)
-        }
-    })
-})
-monitor.textContent.length=9;
+
+
